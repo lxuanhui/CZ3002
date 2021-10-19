2c48158bbf116d34ce1fc24ae5a6a0ee433ad0b9
lxuanhui <xuanhui98@gmail.com>
2021-10-19 15:44:28 +0800

    Set up registration and login

--- CZ3002Project/TrailMakingTest/Authentication/urls.py
+++ CZ3002Project/TrailMakingTest/Authentication/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     # home page should be login page?
     path('', views.home, name='home'),
-    path('login/', views.login, name = 'login'),
-    path('register/', views.register, name = 'register')
+    path('login/', views.LoginPage, name = 'login'),
+    path('register/', views.RegisterPage, name = 'register')
 ]
