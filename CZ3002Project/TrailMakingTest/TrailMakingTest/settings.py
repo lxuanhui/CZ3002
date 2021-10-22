0d46848f84bc51ec6c7f5993a5640f60bc489249
Lee Xuanhui <xuanhui98@gmail.com>
2021-10-22 19:21:47 +0800

    Revert "Home page logo inserted"
    This reverts commit b7225ad7b13a64eaea2dba2cf55aeb2bd5983e43.

--- CZ3002Project/TrailMakingTest/TrailMakingTest/settings.py
+++ CZ3002Project/TrailMakingTest/TrailMakingTest/settings.py
@@ -129,8 +129,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/images/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
