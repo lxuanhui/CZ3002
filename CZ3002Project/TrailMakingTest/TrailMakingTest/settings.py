b7225ad7b13a64eaea2dba2cf55aeb2bd5983e43
Lee Xuanhui <xuanhui98@gmail.com>
2021-10-22 15:41:08 +0800

    Home page logo inserted

--- CZ3002Project/TrailMakingTest/TrailMakingTest/settings.py
+++ CZ3002Project/TrailMakingTest/TrailMakingTest/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
